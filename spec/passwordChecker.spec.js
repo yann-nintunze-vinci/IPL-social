7035e637fb28c0ecf40180f2794c66bde66b887f
Yann Ntz <yann.nintunze@student.vinci.be>
2025-01-17 16:06:15 +0100

    add: dependencies for jasmine and node

---
+++ spec/passwordChecker.spec.js
@@ -0,0 +1,10 @@
+const passwordChecker = require("../passwordChecker");
+
+describe("Password checker", () => {
+    const result = passwordChecker;
+
+    it("should return 2 when 1 + 1", () => {
+
+        expect(1 + 1).toBe(2);
+    })
+})
