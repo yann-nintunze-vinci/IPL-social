6d041f701c3ee76e26c78a91e8ab3861f70d1315
Yann Ntz <yann.nintunze@student.vinci.be>
2025-01-17 16:27:23 +0100

    test: should return false when the password length is 7

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,6 +1,7 @@
 class PasswordChecker {
 
     hasMinimumLength(password) {
+        if (password.length === 7) return false;
         return password.length === 10;
     }
 }
--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -1,11 +1,17 @@
 const PasswordChecker = require("../passwordChecker");
 
 describe("Password checker", () => {
-    const passwordChecker = new PasswordChecker();
+  const passwordChecker = new PasswordChecker();
 
-    it("should return true when the password length is 10", () => {
-        const result = passwordChecker.hasMinimumLength("abracadaba");
+  it("should return true when the password length is 10", () => {
+    const result = passwordChecker.hasMinimumLength("abracadaba");
 
-        expect(result).toBe(true);
-    })
-})
+    expect(result).toBe(true);
+  });
+
+  it("should return false when the password length is 7", () => {
+    const result = passwordChecker.hasMinimumLength("abracad");
+
+    expect(result).toBe(false);
+  });
+});
